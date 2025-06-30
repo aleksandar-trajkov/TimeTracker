6728e71da365fc379f97c5d09f44874ef76d72db
Ace Trajkov <aleksandar.trajkov@outlook.com>
2025-07-01 01:33:49 +0200

    Added sri

--- TimeTracker.Frontend.React/vite.config.ts
+++ TimeTracker.Frontend.React/vite.config.ts
@@ -1,18 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import subresourceIntegrity from 'vite-plugin-sri'
 
 // https://vite.dev/config/
 export default defineConfig({
   define: {
     BUILD_DATE: JSON.stringify(new Date().valueOf()),
   },
-  plugins: [react()],
+  plugins: [
+	react(),
+	subresourceIntegrity()
+  ],
   build: {
     rollupOptions: {
       output: {
-        entryFileNames: `assets/script.js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`
+        entryFileNames: `assets/script-[hash].js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+        assetFileNames: `assets/[name]-[hash].[ext]`
       }
     }
   }
